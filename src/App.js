1f641992b2a849d18229d4fd4e78c97efdbdf1e1
p-miszczuk <piotr.miszczuk@interia.pl>
2019-09-23 20:25:13 +0200

    add scss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './containers/Header'
 import Main from './containers/Main'
 import store from './redux/store/appStore'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './taskStyle.css'
+import './taskStyle.scss'
 
 const Task = ({ task }) => {
   const {
