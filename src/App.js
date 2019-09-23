a06e372f9a30a78819ef77d128d3fea99f3cc1a0
p-miszczuk <piotr.miszczuk@interia.pl>
2019-09-23 13:29:29 +0200

    simple welcome view and menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+import Index from './components/Index'
+import List from './components/List'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/list/">List</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Route path="/" exact component={Index} />
+          <Route path="/list/" component={List} />
+        </main>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
