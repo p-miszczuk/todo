9d1ebde1a4a2ab14332258ca4767783381e216ef
p-miszczuk <piotr.miszczuk@interia.pl>
2019-09-23 14:31:59 +0200

    add new components, change Link to NavLink

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './containers/Header'
+import Main from './containers/Main'
 import './App.css'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom'
-import Index from './components/Index'
-import List from './components/List'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/list/">List</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          <Route path="/" exact component={Index} />
-          <Route path="/list/" component={List} />
-        </main>
+        <Header />
+        <Main />
       </div>
     </Router>
   )
