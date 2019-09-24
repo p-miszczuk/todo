0cdf4403e31c5656004e95c085d094d4de72205b
p-miszczuk <piotr.miszczuk@interia.pl>
2019-09-24 20:54:19 +0200

    update structure

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux'
-import todos from '../reducers/reducer'
+import todos from '../reducers/todos/reducer'
 
 const rootReducer = combineReducers({ todos })
 const store = createStore(rootReducer)
