3527bc1177e2f3045f12eab71b6f3f210478e08e
p-miszczuk <piotr.miszczuk@interia.pl>
2019-09-26 13:54:12 +0200

    update sort logic

--- src/components/List.js
+++ src/components/List.js
@@ -65,7 +65,10 @@ class List extends React.Component {
               />
             ))
             .reverse()
-            .sort((x, y) => x.props.task.done - y.props.task.done)}
+            .sort(
+              (taskA, taskB) =>
+                taskA.props.task.done - taskB.props.task.done,
+            )}
           <SetFormBody
             closeEditForm
             showPopup={showPopup}
